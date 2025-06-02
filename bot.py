48e966b703dd4c449279be78715da740747d60fa
Not faund <flaelva6@gmail.com>
2025-06-02 18:52:15 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 @app.on_message(filters.command("start"))
 def start(client, message):
-    message.reply("Selamat datang! Kirimkan URL yang ingin Anda cek.")
+    message.reply("welcome silahkan kirim link anda untuk di chek apa lah ke DDoS atau tidak")
 
 @app.on_message(filters.text)
 def check_website(client, message):
